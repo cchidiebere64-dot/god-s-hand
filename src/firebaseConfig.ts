48ad210c371dcecb4c39aa298688ad5c5cec5e7e
cchidiebere64-dot <cchidiebere64@gmail.com>
2025-08-28 02:29:56 -0700

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,5 +1,5 @@
 // src/firebaseConfig.ts
-{/*import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
@@ -18,4 +18,5 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
-*/}
+
+
